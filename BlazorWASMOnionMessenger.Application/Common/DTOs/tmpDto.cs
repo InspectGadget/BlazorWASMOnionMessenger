8cbec92f802667c093cb24099bc0e9c9295431dc
Andrii Liashovskyi <sparlolaa@gmail.com>
2023-10-26 17:35:22 +0300

    Added tmp files into empty folders to be able to see folders structure

---
+++ BlazorWASMOnionMessenger.Application/Common/DTOs/tmpDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWASMOnionMessenger.Application.Common.DTOs
+{
+    internal class tmpDto
+    {
+    }
+}
---
+++ BlazorWASMOnionMessenger.tmpTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace BlazorWASMOnionMessenger.tmpTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
